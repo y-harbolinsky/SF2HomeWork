ae10ef24554c37f2067fe569fc9eebb815dfab2b
green <greenua14@gmail.com>
2015-01-11 22:08:53 +0200

    fix services.xml loader

--- src/Blog/NewsBundle/DependencyInjection/BlogNewsExtension.php
+++ src/Blog/NewsBundle/DependencyInjection/BlogNewsExtension.php
@@ -2,20 +2,17 @@
 
 namespace Blog\NewsBundle\DependencyInjection;
 
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Test\TestBundle\DependencyInjection\Configuration;
 
-class NewsExtension extends Extension
+class BlogNewsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder)
     {
-        $configuration = new Configuration();
-        $config = $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new XmlFileLoader($containerBuilder, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('services.xml');
     }
 }
